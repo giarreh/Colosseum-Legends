7c019032cc24a5df4a8dc478e1c634d3cadd1a61
Giar Rehani <giarreh1@gmail.com>
2024-02-24 16:45:59 +0100

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header className="header-container">
+      <h1 className="header-item1">My First React App</h1>
+      <h1 className="header-item2">My First React App</h1>
+      <h1 className="header-item3">My First React App</h1>
+    </header>
+  );
+}
